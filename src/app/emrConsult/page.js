e4c3723d138ef59c62b838a6bc11ccd23b9f1ce8
uxmason <uxmason@me.com>
2025-04-17 19:01:52 +0900

    ...

--- src/app/emrConsult/page.js
+++ src/app/emrConsult/page.js
@@ -1,5 +1,4 @@
 "use client";
-export const dynamic = 'force-dynamic';
 import Image from 'next/image'
 import styles from './page.module.css'
 import ManagerInfoTicker from '../components/ManagerInfoTicker'
